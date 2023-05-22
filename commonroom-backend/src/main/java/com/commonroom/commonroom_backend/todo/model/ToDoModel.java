bab3f1194ab1a9edb67e3e665889cafc725270fc
beyzAbdullahoglu <abdullahoglubeyza18@gmail.com>
2023-05-22 22:45:13 +0300

    commit3

--- commonroom-backend/src/main/java/com/commonroom/commonroom_backend/todo/model/ToDoModel.java
+++ commonroom-backend/src/main/java/com/commonroom/commonroom_backend/todo/model/ToDoModel.java
@@ -2,6 +2,6 @@ package com.commonroom.commonroom_backend.todo.model;
 
 public class ToDoModel {
     public static void main(String args[]){
-        System.out.println("hellü");
+        System.out.println("hello world");
     }
 }
